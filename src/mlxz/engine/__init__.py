54b53ef2d4d0aa78867ac62e8b233ba8e32aab4c
MZ <mz@wfca.com>
2026-04-27 08:50:27 -0700

    ci: make lint advisory and lazy-load exports

--- src/mlxz/engine/__init__.py
+++ src/mlxz/engine/__init__.py
@@ -1,12 +1,15 @@
 """Inference engine implementations."""
 
-from mlxz.engine.continuous import ContinuousBatchingEngine
-from mlxz.engine.draft import DraftModel
-from mlxz.engine.request import Request, Token
-from mlxz.engine.sampling import sample
-from mlxz.engine.single_stream import SingleStreamEngine
-from mlxz.engine.speculative import SpeculativeEngine
-from mlxz.engine.thread_boundary import CancellationRegistry, MxEvalGuard, RequestBridge
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from mlxz.engine.continuous import ContinuousBatchingEngine
+    from mlxz.engine.draft import DraftModel
+    from mlxz.engine.request import Request, Token
+    from mlxz.engine.sampling import sample
+    from mlxz.engine.single_stream import SingleStreamEngine
+    from mlxz.engine.speculative import SpeculativeEngine
+    from mlxz.engine.thread_boundary import CancellationRegistry, MxEvalGuard, RequestBridge
 
 __all__ = [
     "CancellationRegistry",
@@ -20,3 +23,47 @@ __all__ = [
     "Token",
     "sample",
 ]
+
+
+def __getattr__(name: str) -> Any:
+    if name == "ContinuousBatchingEngine":
+        from mlxz.engine.continuous import ContinuousBatchingEngine
+
+        return ContinuousBatchingEngine
+    if name == "DraftModel":
+        from mlxz.engine.draft import DraftModel
+
+        return DraftModel
+    if name == "Request":
+        from mlxz.engine.request import Request
+
+        return Request
+    if name == "Token":
+        from mlxz.engine.request import Token
+
+        return Token
+    if name == "sample":
+        from mlxz.engine.sampling import sample
+
+        return sample
+    if name == "SingleStreamEngine":
+        from mlxz.engine.single_stream import SingleStreamEngine
+
+        return SingleStreamEngine
+    if name == "SpeculativeEngine":
+        from mlxz.engine.speculative import SpeculativeEngine
+
+        return SpeculativeEngine
+    if name == "CancellationRegistry":
+        from mlxz.engine.thread_boundary import CancellationRegistry
+
+        return CancellationRegistry
+    if name == "MxEvalGuard":
+        from mlxz.engine.thread_boundary import MxEvalGuard
+
+        return MxEvalGuard
+    if name == "RequestBridge":
+        from mlxz.engine.thread_boundary import RequestBridge
+
+        return RequestBridge
+    raise AttributeError(name)
